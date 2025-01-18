8930908c991b0cef29fd3b92e57dd798f9decd47
GeebergyThree <minterproorg@gmail.com>
2025-01-18 10:31:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -298,7 +298,7 @@ async def main():
     first_server_url = "https://api-proxy-5iic.onrender.com"  # Update to actual address
     second_server_url = "https://backend-auth-bcv6.onrender.com"  # Update to actual address
     third_server_url = "https://group-bot-8l89.onrender.com"  # Update to actual address
-    fourth_server_url = "https://meme-scraper-nzty.onrender.com"  # Update to actual address
+    fourth_server_url = "https://meme-scraper-vz0i.onrender.com"  # Update to actual address
     asyncio.create_task(general_health_check(first_server_url, second_server_url, third_server_url, fourth_server_url))
 
     # Keep the bot running, ensuring it doesn't exit prematurely
